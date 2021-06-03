0bffb5e8d8f8f1ce6badb656392e17310f92d0b0
Eden Mendez <edenml264@gmail.com>
2021-06-02 18:21:31 -0600

    Create Mixins & Video background

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,28 @@
-const { series, parallel } = require('gulp');
+const { series, src, dest, watch } = require('gulp');
+const sass = require('gulp-sass');
 
+// Función que compila SASS
+
+function css( ) {
+    return src('src/scss/app.scss')
+        .pipe( sass({
+            outputStyle: 'expanded'
+        }) )
+        .pipe( dest('./build/css') )
+}
+
+function minificarcss( ) {
+    return src('src/scss/app.scss')
+        .pipe( sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe( dest('./build/css') )
+}
+
+function watchArchivos() {
+    watch( 'src/scss/**/*.scss', css );
+}
+
+exports.css = css;
+exports.minificarcss = minificarcss;
+exports.watchArchivos = watchArchivos;
