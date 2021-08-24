ceb1e79659a76c2c975e805b3c8d268d03d7dabb
Eden Mendez <edenml264@gmail.com>
2021-08-24 01:01:50 -0600

    Add festival section

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 const { series, src, dest, watch } = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
 
 // Función que compila SASS
 
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss'
+}
+
 function css( ) {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe( sass({
             outputStyle: 'expanded'
         }) )
@@ -12,17 +20,33 @@ function css( ) {
 }
 
 function minificarcss( ) {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe( sass({
             outputStyle: 'compressed'
         }))
         .pipe( dest('./build/css') )
 }
 
+function imagenes() {
+    return src(paths.imagenes)
+    .pipe( imagemin() )
+    .pipe( dest( './build/img'))
+    .pipe( notify( {message: 'Imagen Minificada'}) );
+}
+function versionWebp() {
+    return src(paths.imagenes)
+    .pipe( webp() )
+    .pipe( dest('./build/img'))
+    .pipe( notify({message: 'Versión webP lista'}) );
+}
+
 function watchArchivos() {
-    watch( 'src/scss/**/*.scss', css );
+    watch( paths.scss, css );
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+
+exports.default = series( css, imagenes, versionWebp, watchArchivos);
